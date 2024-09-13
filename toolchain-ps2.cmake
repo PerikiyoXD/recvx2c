17ad47ad7bc22af1bb5d31dd93f38ee57acc97c0
PerikiyoXD <perikiyoxd@gmail.com>
2024-09-13 19:57:47 +0200

    Finalmente

--- toolchain-ps2.cmake
+++ toolchain-ps2.cmake
@@ -1,5 +1,8 @@
 # toolchain-ps2.cmake
 
+# Enable compile commands for IDEs
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
 # Set the system name
 set(CMAKE_SYSTEM_NAME Generic)
 
@@ -7,10 +10,11 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(PS2_COMPILER_DIR "C:/Users/pxd/Desktop/compilers/mwcps2-2.3.3-000906")
 set(CMAKE_C_COMPILER "${PS2_COMPILER_DIR}/mwccps2.exe")
 set(CMAKE_CXX_COMPILER "${PS2_COMPILER_DIR}/mwccps2.exe")
+set(CMAKE_LINKER "${PS2_COMPILER_DIR}/mwldps2.exe")
 
-# Ensure --gcc-compat is the first flag for the compiler
-set(CMAKE_C_FLAGS_INIT "--gcc-compat")
-set(CMAKE_CXX_FLAGS_INIT "--gcc-compat")
+# Ensure --gcc-compat is the first flag for the compiler and use -c to compile only
+set(CMAKE_C_FLAGS_INIT "--gcc-compat -c")
+set(CMAKE_CXX_FLAGS_INIT "--gcc-compat -c")
 
 # Prevent CMake from checking the compiler
 set(CMAKE_C_COMPILER_WORKS 1)
@@ -18,13 +22,9 @@ set(CMAKE_CXX_COMPILER_WORKS 1)
 
 # Handle include directories
 if(DEFINED ENV{MWCIncludes})
-    # Retrieve the include paths from the environment variable
     set(MWCIncludes_LIST $ENV{MWCIncludes})
-
-    # Iterate over each include directory and append -I"<path>"
     foreach(include_dir IN LISTS MWCIncludes_LIST)
-        # Escape backslashes and quotes if necessary
-        string(REPLACE "\\" "/" include_dir "${include_dir}")  # Replace backslashes with forward slashes
+        string(REPLACE "\\" "/" include_dir "${include_dir}")  # Normalize slashes
         set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -I\"${include_dir}\"")
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -I\"${include_dir}\"")
     endforeach()
@@ -32,18 +32,14 @@ endif()
 
 # Handle library directories
 if(DEFINED ENV{MWLibraries})
-    # Retrieve the library paths from the environment variable
     set(MWLibraries_LIST $ENV{MWLibraries})
-
-    # Iterate over each library directory and append -L"<path>"
     foreach(library_dir IN LISTS MWLibraries_LIST)
-        # Escape backslashes and quotes if necessary
-        string(REPLACE "\\" "/" library_dir "${library_dir}")  # Replace backslashes with forward slashes
+        string(REPLACE "\\" "/" library_dir "${library_dir}")  # Normalize slashes
         set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -L\"${library_dir}\"")
     endforeach()
 endif()
 
-# Set additional linker flags
+# Set linker flags for mwldps2
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -lcf \"${CMAKE_SOURCE_DIR}/linker.lcf\"")
 
 # Set the C standard
