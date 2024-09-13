de18f88bc71a4f0928a5904ef3e39ccaa789bfcf
PerikiyoXD <perikiyoxd@gmail.com>
2024-09-13 19:26:08 +0200

    Agane

--- cmake/toolchain-ps2.cmake
+++ cmake/toolchain-ps2.cmake
@@ -1,34 +1,41 @@
 # toolchain-ps2.cmake
 
-# Set the system name to Generic since PS2 is not recognized by CMake
+# Set the system name
 set(CMAKE_SYSTEM_NAME Generic)
 
-# Specify the path to your Metrowerks PS2 compiler directory
-set(PS2_COMPILER_DIR "C:/Users/pxd/Desktop/compilers/mwcps2-2.3.3-000906")
-
 # Specify the compiler and linker executables
+set(PS2_COMPILER_DIR "C:/Users/pxd/Desktop/compilers/mwcps2-2.3.3-000906")
 set(CMAKE_C_COMPILER "${PS2_COMPILER_DIR}/mwccps2.exe")
-set(CMAKE_LINKER     "${PS2_COMPILER_DIR}/mwldps2.exe")
+set(CMAKE_CXX_COMPILER "${PS2_COMPILER_DIR}/mwccps2.exe")  # If you need C++ support
 
-# Prevent CMake from trying to use a C++ compiler
-set(CMAKE_CXX_COMPILER_WORKS 1)
+# Prevent CMake from checking the compiler
 set(CMAKE_C_COMPILER_WORKS 1)
+set(CMAKE_CXX_COMPILER_WORKS 1)
 
-# Set the assembler to avoid CMake errors (even if not used)
-set(CMAKE_ASM_COMPILER "${CMAKE_C_COMPILER}")
+# Include directories from environment variable
+if(DEFINED ENV{MWCIncludes})
+    file(TO_CMAKE_PATH "$ENV{MWCIncludes}" MWCIncludes_STR)
+    string(REPLACE ";" "|" MWCIncludes_STR "${MWCIncludes_STR}")
+    string(REPLACE "|" ";" MWCIncludes_LIST "${MWCIncludes_STR}")
+    foreach(include_dir ${MWCIncludes_LIST})
+        include_directories("${include_dir}")
+    endforeach()
+endif()
 
-# Define include and library directories
-set(MWCIncludes "C:/usr/local/sce/ee/gcc/ee/include;C:/usr/local/sce/ee/include;C:/Program Files (x86)/Metrowerks/CodeWarrior for PS2 v3.9/PS2 Support")
-set(MWLibraries "C:/usr/local/sce/ee/gcc/ee/lib;C:/usr/local/sce/ee/lib;C:/usr/local/sce/ee/gcc/lib;C:/usr/local/sce/ee/gcc/lib/gcc-lib/ee/2.9-ee-991111-01")
+# Library directories from environment variable
+if(DEFINED ENV{MWLibraries})
+    file(TO_CMAKE_PATH "$ENV{MWLibraries}" MWLibraries_STR)
+    string(REPLACE ";" "|" MWLibraries_STR "${MWLibraries_STR}")
+    string(REPLACE "|" ";" MWLibraries_LIST "${MWLibraries_STR}")
+    foreach(library_dir ${MWLibraries_LIST})
+        link_directories("${library_dir}")
+    endforeach()
+endif()
 
-# Convert paths to CMake format
-file(TO_CMAKE_PATH "${MWCIncludes}" MWCIncludes)
-file(TO_CMAKE_PATH "${MWLibraries}" MWLibraries)
+# Additional linker flags
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -lcf linker.lcf")
 
-# Set compiler and linker flags
-set(CMAKE_C_FLAGS "-I${MWCIncludes}")
-set(CMAKE_EXE_LINKER_FLAGS "-L${MWLibraries} -lcf linker.lcf -entry __start")
-
-# Suppress CMake warnings about missing standards
+# Set the standard
 set(CMAKE_C_STANDARD 99)
+set(CMAKE_C_STANDARD_REQUIRED ON)
 set(CMAKE_C_EXTENSIONS OFF)
