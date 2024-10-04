6db9f7c3da0e2d71db48bfea74ffc81ee3577dbd
PerikiyoXD <perikiyoxd@gmail.com>
2024-10-04 20:36:56 +0200

    Progress on ps2_MemoryCard

--- ps2/veronica/prog/ps2_MemoryCard.h
+++ ps2/veronica/prog/ps2_MemoryCard.h
@@ -3,8 +3,35 @@
 #ifndef _PS2_MEMORYCARD_H_
 #define _PS2_MEMORYCARD_H_
 
-#include "libmc.h"
-#include "memorycard.h"
+#include <libmc.h>
+
+
+typedef struct tagMEMORYCARDPORT {
+    int lCrntType;
+    int lPrevType;
+    int lFreeSize;
+    int lFormatType;
+} MEMORYCARDPORT;
+
+typedef struct tagMEMORYCARDSTATE {
+    unsigned int ulState;
+    unsigned int ulError;
+    unsigned int ulMcSubState;
+    unsigned int ulFileSize;
+    int lCurrentPort;
+    int lOpenFileNumber;
+    int lSelectFileNumber;
+    int lOpenMode;
+    unsigned short usMcSysState;
+    void* vpAddr;
+    char cCurrentDir[50];
+    char cOpenFileName[32];
+    char cRetryCount;
+    char cMcCheckFlag;
+    char cCheckMcFlag;
+    MEMORYCARDPORT Port[2];
+} MEMORYCARDSTATE;
+
 
 MEMORYCARDSTATE* CreateMemoryCard(MEMORYCARDSTATE* pCard);
 int ExecuteMemoryCard(MEMORYCARDSTATE* pCard);
